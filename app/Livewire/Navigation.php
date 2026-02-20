6e9838fb4fa79d9d308495874632197d139a623f
Oscar Martinez M. <oscar.martinez.mora@gmail.com>
2026-02-20 16:36:03 -0600

    Cleaned not needed code after list activations header changes not working tries

--- app/Livewire/Navigation.php
+++ app/Livewire/Navigation.php
@@ -2,7 +2,6 @@
 namespace App\Livewire;
 
 use Illuminate\Support\Facades\Auth;
-use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Navigation extends Component {
@@ -12,28 +11,14 @@ class Navigation extends Component {
   public $sharedLists;
 
   public function mount(): void {
-    $this->updateListData();
-  }
-
-  public function updateListData(): void {
-    $user = Auth::user()->fresh();
+    $user = Auth::user();
     $this->primaryListName = $user->primaryList->name ?? 'Inicio';
     $this->primaryListId = $user->primary_list_id;
     $this->ownedLists = $user->ownedLists;
     $this->sharedLists = $user->sharedLists;
   }
 
-  public function setActiveList(int $listId): void {
-    Auth::user()->update(['primary_list_id' => $listId]);
-    $this->redirect(route('home'), navigate: true);
-  }
-
   public function render() {
     return view('livewire.navigation');
-  }
-
-  #[On('list-changed')]
-  public function refreshNavigation(): void {
-    $this->updateListData();
   }
 }
